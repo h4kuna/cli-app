6bffc85fbfa847eb45ba1c4cc138346b77ad0ebc
Milan Matějček <milan.matejcek@gmail.com>
2019-07-15 07:47:59 +0200

    remove bootstrap.php

---
+++ app/src/Bootstrap.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace h4kuna\Cli\App;
+
+use Nette;
+
+class Bootstrap
+{
+
+	public static function createContainer()
+	{
+		$tempDir = __DIR__ . '/../temp';
+		$debuggerFile = __DIR__ . '/../config/debugger-enable';
+
+		$configurator = new Nette\Configurator;
+
+		if (is_file($debuggerFile)) {
+			$configurator->setDebugMode(TRUE);
+		}
+		$configurator->enableTracy($tempDir);
+		$configurator->setTimeZone('Europe/Prague');
+		$configurator->setTempDirectory($tempDir);
+		$configurator->addConfig([
+			'parameters' => [
+				'debuggerFile' => $debuggerFile,
+			],
+		]);
+
+		$configurator->addConfig(__DIR__ . '/../config/config.neon');
+
+		$localNeon = __DIR__ . '/../config/config.local.neon';
+		!is_file($localNeon) && touch($localNeon);
+		$configurator->addConfig($localNeon);
+
+		$container = $configurator->createContainer();
+		return $container;
+	}
+
+}
