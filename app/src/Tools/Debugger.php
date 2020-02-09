7bb927e1f19731bab7ba268bbd3f857bb07eca01
Milan Matějček <milan.matejcek@gmail.com>
2020-02-09 20:45:06 +0100

    Purge temp if debugger disabled

--- app/src/Tools/Debugger.php
+++ app/src/Tools/Debugger.php
@@ -1,33 +1,41 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace h4kuna\Cli\App\Tools;
 
+use Nette\Utils;
+
 class Debugger
 {
+	/** @var string */
 	private $debuggerFile;
 
+	/** @var string */
+	private $tempDir;
 
-	public function __construct($debuggerFile)
+
+	public function __construct(string $debuggerFile, string $tempDir)
 	{
 		$this->debuggerFile = $debuggerFile;
+		$this->tempDir = $tempDir;
 	}
 
 
-	public function enable()
+	public function enable(): void
 	{
 		touch($this->debuggerFile);
 	}
 
 
-	public function disable()
+	public function disable(): void
 	{
 		if ($this->isEnabled()) {
 			unlink($this->debuggerFile);
+			Utils\FileSystem::delete($this->tempDir);
 		}
 	}
 
 
-	public function isEnabled()
+	public function isEnabled(): bool
 	{
 		return is_file($this->debuggerFile);
 	}
